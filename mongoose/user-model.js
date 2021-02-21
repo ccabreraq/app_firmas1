9773cb0034d0e2268a0195ad40081ab35573836d
ccabreraq <ccabreraq@gmail.com>
2021-02-21 12:30:36 -0500

    Update user-model.js

--- mongoose/user-model.js
+++ mongoose/user-model.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const UserSchema = new Schema({
-  cedula: { type: String, required: true },
+  cedula: { type: String, required: true, unique: true },
    nombres: { type: String, required: true },
   apellidos: { type: String, required: true },
-  celular: { type: String, required: true },
+  celular: { type: String, required: true  },
  email: { type: String, required: true },
-  encryptedPassword: { type: String, required: true },
+  encryptedPassword: { type: String},
   role: { type: String, enum: ['admin', 'restricted'], required: true },
 })
 
